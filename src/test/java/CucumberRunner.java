c50cc56f28e0382ca25c50eaed639e66096ac75b
Andrey Veselkov <kapel1920@rambler.ru>
2018-01-31 21:21:33 +0300

    Initial commit

---
+++ src/test/java/CucumberRunner.java
@@ -0,0 +1,17 @@
+/**
+ * @author Andrey Strelnikov
+ * First AutoTest
+ */
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/resources"}, glue = {"steps"},
+        plugin = {
+                "util.AllureReporter"
+        })
+public class CucumberRunner {}
